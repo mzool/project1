8e1250d3477c578b379fbc2ac15113c4dcb8267a
mzool <mohammad_alzoul97@yahoo.com>
2020-12-23 16:47:53 +0200

    java added

---
+++ java.js
@@ -0,0 +1,6 @@
+alert ("Welcome!");
+ o = prompt("please enter your major");
+ var x = confirm("you want to learn about chemical engineering?");
+ if (x == true) window.open(" https://zaidalasfar97.github.io/nice-one/home.html");
+
+ 
